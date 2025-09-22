593ca73b0f9d068c7060bdec4947c9db98b32b64
Etotaziba Olei <teneeto@gmail.com>
2025-09-22 20:53:10 +0100

    update to use reactquery

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import StockListScreen from './src/screens/StockListScreen.tsx';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle="light-content" />
-      <StockListScreen />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <StatusBar barStyle="light-content" />
+        <StockListScreen />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
